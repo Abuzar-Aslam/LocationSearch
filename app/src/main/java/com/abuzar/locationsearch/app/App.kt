bd2d6b1bf0befa93aac7ade5488d48354d2f646d
Abuzar <abuzar_aslam@astro.com.my>
2021-03-27 19:11:37 +0800

    LS: Added the base App and added the dependency for the koin and kotlin and gson application use ...

---
+++ app/src/main/java/com/abuzar/locationsearch/app/App.kt
@@ -0,0 +1,15 @@
+package com.abuzar.locationsearch.app
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(applicationContext, listOf())
+    }
+
+
+}
