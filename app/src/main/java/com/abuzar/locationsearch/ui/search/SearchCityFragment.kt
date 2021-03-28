43d7c3a5137a77017140ba749c5113da16506da3
Abuzar <abuzar_aslam@astro.com.my>
2021-03-28 20:31:21 +0800

    LS: Implemented the BaseFragment and added the SearchFragment ...

---
+++ app/src/main/java/com/abuzar/locationsearch/ui/search/SearchCityFragment.kt
@@ -0,0 +1,37 @@
+package com.abuzar.locationsearch.ui.search
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.abuzar.locationsearch.R
+import com.abuzar.locationsearch.base.BaseFragment
+import com.abuzar.locationsearch.databinding.SearchCityFragmentBinding
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class SearchCityFragment : BaseFragment<SearchCityFragmentBinding>() {
+
+
+    private val searchViewModel: SearchCityViewModel by viewModel()
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+
+
+    }
+
+    override fun getLayoutId(): Int {
+        return  R.layout.search_city_fragment
+    }
+
+    override fun getViewModel(): ViewModel {
+        return searchViewModel
+    }
+
+}
---
+++ app/src/main/java/com/abuzar/locationsearch/ui/search/SearchCityViewModel.kt
@@ -0,0 +1,7 @@
+package com.abuzar.locationsearch.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchCityViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
